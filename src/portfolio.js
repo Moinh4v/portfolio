d7a9ca455130e87e7547eb281e77cc8c4e818b10
Moinh4v <moinuddin@vume.in>
2025-05-12 13:43:09 +0530

    Add social-links

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,25 +21,25 @@ const illustration = {
 
 const greeting = {
   username: "Moinuddin T A",
-  title: "Hi all, I'm Saad",
+  title: "Hi all, I'm Moinuddin 👋",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "🚀 A passionate DevSecOps Engineer and Cloud Security Specialist with hands-on experience in AWS Infrastructure, Secure CI/CD Pipelines, Kubernetes, Docker, Serverless Applications, and Application Security. I love automating deployments, securing cloud environments, and optimizing infrastructure at scale."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
-  displayGreeting: true // Set false to hide this section, defaults to true
+    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing",
+  displayGreeting: true
 };
 
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  github: "https://github.com/Moinh4v",
+  linkedin: "https://www.linkedin.com/in/moinuddin-devsecops/",
+  gmail: "mointa97@gmail.com",
+  // gitlab: "https://gitlab.com/saadpasta",
+  // facebook: "https://www.facebook.com/saad.pasta7",
+  // medium: "https://medium.com/@saadpasta",
+  // stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
